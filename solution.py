af2dbf413505d844573b879e5f680dd0645d6c3e
Aynur Gatiyatullin <vildend@mail.ru>
2023-04-06 18:20:59 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,6 @@ import numpy as np
 chat_id = 897901830
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    t = 22
+    l = x.mean()
+    return (np.power(l * t, x) * np.exp(-(l * t))) / scipy.special.factorial(x)
