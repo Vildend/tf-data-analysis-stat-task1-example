a8458e4840de0d36e2dbdd28e52be2ec3d8a95bf
Aynur Gatiyatullin <vildend@mail.ru>
2023-04-07 12:29:05 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import numpy as np
-import scipy.special
 
 chat_id = 897901830
 
 def solution(x: np.array) -> float:
     t = 22
     l = x.mean()
-    return (np.power(l * t, len(x)) * np.exp(-(l * t))) / scipy.special.factorial(len(x))
+    return l * t
